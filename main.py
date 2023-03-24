d358c3de13a21c2998c01a3836bfec020c48e4c6
Sena <36813961+helenaferdy@users.noreply.github.com>
2023-03-24 17:25:50 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from pyvis.network import Network
+
+# create a network
+net = Network()
+
+# add nodes
+net.add_node(1, label="Node 1")
+net.add_node(2, label="Node 2")
+net.add_node(3, label="Node 3")
+net.add_node(4, label="Node 4")
+net.add_edges([(1,2)])
+net.add_edges([(1,3)])
+net.add_edges([(1,4)])
+net.add_edges([(2,1)])
+net.add_edges([(2,3)])
+net.add_edges([(1,4)])
+
+# show the network
+net.show("my_network.html")
+
+from IPython.core.display import display, HTML
+display(HTML("my_network.html"))
